26f66636564c7a058f3e3074cea2e0dcb2a2d336
Yijiang Huang <yijiangh@mit.edu>
2018-09-24 10:59:41 -0400

    add a boolean toggle for cmake building pybindings.

--- src/eigen_demo/eigen_demo.cpp
+++ src/eigen_demo/eigen_demo.cpp
@@ -1,30 +1,29 @@
-//#include "eigen_demo/eigen_demo_IO.cpp"
+//
+// Created by yijiangh on 9/24/18.
+//
+
+#ifndef CONMECH_EIGEN_DEMO_H
+#define CONMECH_EIGEN_DEMO_H
 
 #include <string>
 #include <iostream>
 
 #include <eigen3/Eigen/Dense>
+#include "eigen_demo/eigen_demo.hpp"
 
-class EigenSolveDemo
+void EigenSolveDemo::testEigen()
 {
- public:
-  EigenSolveDemo(){}
+  using namespace std;
+  using namespace Eigen;
 
-  void testEigen()
-  {
-    using namespace std;
-    using namespace Eigen;
+  Matrix3f A;
+  Vector3f b;
+  A << 1,2,3,  4,5,6,  7,8,10;
+  b << 3, 3, 4;
+  cout << "Here is the matrix A:\n" << A << endl;
+  cout << "Here is the vector b:\n" << b << endl;
+  Vector3f x = A.colPivHouseholderQr().solve(b);
+  cout << "The solution is:\n" << x << endl;
+}
 
-    Matrix3f A;
-    Vector3f b;
-    A << 1,2,3,  4,5,6,  7,8,10;
-    b << 3, 3, 4;
-    cout << "Here is the matrix A:\n" << A << endl;
-    cout << "Here is the vector b:\n" << b << endl;
-    Vector3f x = A.colPivHouseholderQr().solve(b);
-    cout << "The solution is:\n" << x << endl;
-  }
-
- private:
-  std::string file_path;
-};
+#endif //CONMECH_EIGEN_DEMO_H
---
+++ src/eigen_demo/eigen_demo.hpp
@@ -0,0 +1,14 @@
+//#include "eigen_demo/eigen_demo_IO.cpp"
+
+#include <string>
+
+class EigenSolveDemo
+{
+ public:
+  EigenSolveDemo(){}
+
+  void testEigen();
+
+ private:
+  std::string file_path_;
+};
