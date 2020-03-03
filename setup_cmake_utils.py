c9a1573e5abb0763f93f864c0039f5211c121afb
yijiangh <yijiangh@mit.edu>
2020-03-03 13:24:49 -0500

    cmake find customized python path

--- setup_cmake_utils.py
+++ setup_cmake_utils.py
@@ -43,10 +43,18 @@ class CMakeBuild(build_ext):
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        execdir = sys.exec_prefix
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable,
+                      '-DPYTHON_LIBRARY=' + os.path.join(execdir, 'lib', ),
+                      '-DPYTHON_INCLUDE_DIR=' + ,
                       '-DCONMECH_BUILD_TESTS=OFF']
                       # disable cpp test because we exclude them in MANIFEST.in
+
+        # build against the non-system (homebrew, etc.) Python install
+        # https://github.com/pybind/pybind11/issues/99#issuecomment-207782425
+        # cmake -DPYTHON_LIBRARY=$(python-config --prefix)/lib/libpython2.7.dylib 
+        #       -DPYTHON_INCLUDE_DIR=$(python-config --prefix)/include/python2.7 .
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
