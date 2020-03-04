a31a65be8f8cb7c953d2577ae4cb697e14b989d3
yijiangh <yijiangh@mit.edu>
2020-03-04 00:08:49 -0500

    ci patch again

--- tests/python/test_parsing.py
+++ tests/python/test_parsing.py
@@ -1,9 +1,5 @@
 import os
 import sys
-if sys.version_info[0] < 3:
-    from backports import tempfile
-else:
-    import tempfile
 from numpy.testing import assert_equal, assert_almost_equal
 import pytest
 import random
@@ -19,13 +15,12 @@ def test_frame_file_io(test_data_dir):
     node_points, element_vids, fix_specs, model_type, material_dicts, model_name, unit = \
         read_frame_json(file_path, verbose=True)
 
-    # temp_fp = os.path.join(here, 'tmp.json')
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_fp = os.path.join(temp_dir, file_name)
-        write_frame_json(temp_fp, node_points, element_vids, fix_specs, material_dicts, \
-            model_type=model_type, model_name=model_name, unit=unit)
-        back_node_points, back_element_vids, back_fix_specs, back_model_type, back_material_dicts, back_model_name, back_unit = \
-            read_frame_json(temp_fp, verbose=True)
+    temp_dir = os.path.dirname(os.path.abspath(__file__))
+    temp_fp = os.path.join(temp_dir, 'tmp_'+file_name)
+    write_frame_json(temp_fp, node_points, element_vids, fix_specs, material_dicts, \
+        model_type=model_type, model_name=model_name, unit=unit)
+    back_node_points, back_element_vids, back_fix_specs, back_model_type, back_material_dicts, back_model_name, back_unit = \
+        read_frame_json(temp_fp, verbose=True)
 
     for n1, n2 in zip(node_points, back_node_points):
         assert_equal(n1, n2)
@@ -56,31 +51,31 @@ def test_parse_material_properties_from_frame_json(test_data_dir):
     assert str(excinfo.value) == err_msg
 
     def check_mat(i, mat_entry, err_msg, expect_failure=True):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            temp_fp = os.path.join(temp_dir, file_name)
-            mds = deepcopy(material_dicts)
-            if i is not None:
-                uniform = False
-                # err_msg = 'Element {}: '.format(i) + err_msg
-            else:
-                i = 0
-                uniform = True
+        temp_dir = os.path.dirname(os.path.abspath(__file__))
+        temp_fp = os.path.join(temp_dir, 'tmp_'+file_name)
+        mds = deepcopy(material_dicts)
+        if i is not None:
+            uniform = False
+            # err_msg = 'Element {}: '.format(i) + err_msg
+        else:
+            i = 0
+            uniform = True
 
-            if isinstance(mat_entry, list):
-                for m in mat_entry:
-                    del mds[i][m]
-            else:
-                del mds[i][mat_entry]
+        if isinstance(mat_entry, list):
+            for m in mat_entry:
+                del mds[i][m]
+        else:
+            del mds[i][mat_entry]
 
-            write_frame_json(temp_fp, node_points, element_vids, fix_specs, mds, \
-              unif_cross_sec=uniform, unif_material=uniform, \
-              model_type=model_type, model_name=model_name, check_material=False, unit=unit)
-            if expect_failure:
-                with pytest.raises(RuntimeError) as excinfo:
-                    StiffnessChecker.from_json(json_file_path=temp_fp)
-                assert str(excinfo.value) == err_msg
-            else:
+        write_frame_json(temp_fp, node_points, element_vids, fix_specs, mds, \
+          unif_cross_sec=uniform, unif_material=uniform, \
+          model_type=model_type, model_name=model_name, check_material=False, unit=unit)
+        if expect_failure:
+            with pytest.raises(RuntimeError) as excinfo:
                 StiffnessChecker.from_json(json_file_path=temp_fp)
+            assert str(excinfo.value) == err_msg
+        else:
+            StiffnessChecker.from_json(json_file_path=temp_fp)
 
     for test_element in [True, False]:
         i = random.choice(list(range(len(element_vids)))) if test_element else None
